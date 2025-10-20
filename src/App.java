b2ed03ff6bd2cc82db6992e8274542889485521d
Alejandro Flores <dev.alejandro.flores@gmail.com>
2025-10-19 19:08:01 -0700

    base ui

---
+++ src/App.java
@@ -0,0 +1,7 @@
+import ui.LexerWindow;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        new LexerWindow();
+    }
+}
---
+++ src/ui/LexerWindow.java
@@ -0,0 +1,14 @@
+package ui;
+
+import javax.swing.*;
+
+public class LexerWindow extends JFrame {
+  public LexerWindow() {
+    super("Analizador lexico");
+
+    setSize(1920,1080);
+    setResizable(false);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setVisible(true);
+  }
+}
