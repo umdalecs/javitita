189ef046e5e4ecae3aa8f6485b2a56232ad661a3
Alejandro Flores <dev.alejandro.flores@gmail.com>
2026-03-16 15:27:31 -0700

    feat: intermediate code finished

--- app/src/main/java/com/umdalecs/javitita/compiler/intermediate/IntermediateCodeGenerator.java
+++ app/src/main/java/com/umdalecs/javitita/compiler/intermediate/IntermediateCodeGenerator.java
@@ -1,6 +1,8 @@
 package com.umdalecs.javitita.compiler.intermediate;
 
 import com.umdalecs.javitita.compiler.SymbolTable;
+import com.umdalecs.javitita.compiler.lexer.TokenType;
+import com.umdalecs.javitita.compiler.parser.Type;
 import com.umdalecs.javitita.compiler.parser.statements.PrintStatement;
 import com.umdalecs.javitita.compiler.parser.statements.VarAssignStatement;
 import com.umdalecs.javitita.compiler.parser.statements.WhileStatement;
@@ -9,14 +11,6 @@ import com.umdalecs.javitita.compiler.parser.syntaxtree.Program;
 import com.umdalecs.javitita.compiler.parser.syntaxtree.Statement;
 
 public class IntermediateCodeGenerator {
-    /**
-     * label - instruction  - operands - comment
-     * */
-    private static final String lineFormat = "%-10s %-14s %-20s\n";
-    /**
-     * name - size  - initial value - comment
-     * */
-    private static final String symbolFormat = "%-10s %-14s %-20s\n";
     private final SymbolTable symbolTable;
     private final Program program;
     private final StringBuilder builder;
@@ -31,31 +25,35 @@ public class IntermediateCodeGenerator {
 
 
     public String generate() {
-        builder.append(String.format("%-10s %-14s\n", "section",".text"));
-        builder.append(String.format("%-10s %-14s\n", "global","_start"));
-
+        builder.append(String.format("%-10s %-14s %s\n", "","TITLE", "TEST1"));
+        builder.append(String.format("%-10s %-14s %s\n", "",".model", "SMALL"));
+        builder.append(String.format("%-10s %-14s %s\n", "",".stack", "100h"));
 
         generateSymbolTable();
 
-//        builder.append(String.format("%-10s %-14s\n", "", ".CODE"));
-        builder.append("_start:\n");
+        builder.append(String.format("%-10s %s\n", "", ".code"));
+        builder.append("main:\n");
 
         for (var statement: program.getStatements()) {
             generateStatement(statement);
         }
 
+        builder.append("""
+                           MOV            AX, 4C00H
+                           INT            21H
+                """);
+        builder.append("end main\n");
+
         return builder.toString();
     }
 
     public void generateSymbolTable() {
-        builder.append(String.format("%-10s %-14s\n", "", ".DATA"));
+        builder.append(String.format("%-10s %s\n", "", ".data"));
 
         for (var symbol: symbolTable.getValues()) {
-            var size = switch (symbol.type()) {
-                case BOOLEAN -> "DW";
-                case INTEGER -> "DD";
-            };
-            builder.append(String.format(symbolFormat,
+            var size = "dw";
+
+            builder.append(String.format("%-10s %-14s %s\n",
                     symbol.token().literal(),
                     size,
                     "?"));
@@ -66,55 +64,141 @@ public class IntermediateCodeGenerator {
 
         switch (st) {
             case WhileStatement ws -> {
-                String format = "";
+                String e1 = "E" + contadorEtiqueta++;
+                String e2 = "E" + contadorEtiqueta++;
+
+                var result = checkExpressionResult(ws.getCondition());
+
+                builder.append(String.format("""
+                        %s:
+                                   CMP            %s, 0
+                                   JE             %s
+                        """, e1, result, e2));
 
                 for (var statement: ws.getStatements()) {
                     generateStatement(statement);
                 }
 
+                builder.append(String.format("""
+                                   JP             %s
+                        %s:
+                        """, e1, e2));
+
             }
             case PrintStatement ps -> {
                 String result = checkExpressionResult(ps.getExpression());
+
+                String e1 = "E" + contadorEtiqueta++;
+                String e2 = "E" + contadorEtiqueta++;
 
                 String format = """
                                    MOV            AX, %s
                                    MOV            BX, 10
                                    XOR            CX, CX
-                        E%-8s
+                        %s:
                                    XOR            DX, DX
                                    DIV            BX
                                    PUSH           DX
                                    INC            CX
                                    CMP            AX, 0
-                                   JNE            E%-8s
-                        E%-8s
+                                   JNE            %s
+                        %s:
                                    POP            DX
                                    ADD            DL, '0'
                                    MOV            AH, 02H
                                    INT            21H
-                                   LOOP           E%-8s
+                                   LOOP           %s
                         """;
                 builder.append(String.format(format,
                         result,
-                        ++contadorEtiqueta + ":",
-                        contadorEtiqueta,
-                        ++contadorEtiqueta + ":",
-                        contadorEtiqueta));
+                        e1, e1, e2, e2));
             }
             case VarAssignStatement vas -> {
-                String var = vas.getIdentifier().literal();
                 String result = checkExpressionResult(vas.getExpression());
+
+                var symbol = symbolTable.getSymbol(vas.getIdentifier().literal());
 
                 String format = "           MOV            %s, %s\n";
                 builder.append(String.format(format,
-                        var,
+                        "word ptr [" + symbol.token().literal() + "]",
                         result));
             }
             default -> {}
         }
     }
 
+
+
     private String checkExpressionResult(Expression expression) {
-        return "12396";
+        if (expression.getType() == Type.BOOLEAN){
+            if (expression.getLeft().literal().equals("true"))
+                return "1";
+            else if (expression.getLeft().literal().equals("false"))
+                return "0";
+            else {
+                var left =  expression.getLeft().type() == TokenType.IDENTIFIER
+                        ? "WORD PTR [" + expression.getLeft().literal() + "]"
+                        : expression.getLeft().literal();
+
+                var right =  expression.getRight().type() == TokenType.IDENTIFIER
+                        ? "WORD PTR [" + expression.getRight().literal() + "]"
+                        : expression.getRight().literal();
+
+                if (expression.getOperation().type() == TokenType.LOWER_THAN) {
+                    builder.append(String.format("""
+                                       MOV            AX, %s
+                                       MOV            BX, %s
+                                       CMP            AX, BX
+                                       SETL           AX
+                            """, left, right));
+                }
+                return "AX";
+            }
+        } else {
+            if (expression.getOperation() == null){
+                if (expression.getLeft().type() == TokenType.IDENTIFIER){
+                    return "WORD PTR [" + expression.getLeft().literal() + "]";
+                } else {
+                    return expression.getLeft().literal();
+                }
+            }else {
+                var left =  expression.getLeft().type() == TokenType.IDENTIFIER
+                        ? "WORD PTR [" + expression.getLeft().literal() + "]"
+                        : expression.getLeft().literal();
+
+                var right =  expression.getRight().type() == TokenType.IDENTIFIER
+                        ? "WORD PTR [" + expression.getRight().literal() + "]"
+                        : expression.getRight().literal();
+
+                switch (expression.getOperation().type()) {
+                    case PLUS: {
+                        builder.append(String.format("""
+                                       MOV            AX, %s
+                                       MOV            BX, %s
+                                       ADD            AX, BX
+                            """, left, right));
+                    }
+                    break;
+                    case MINUS: {
+                        builder.append(String.format("""
+                                       MOV            AX, %s
+                                       MOV            BX, %s
+                                       SUB            AX, BX
+                            """, left, right));
+                    }
+                    break;
+                    case MULTI: {
+                        builder.append(String.format("""
+                                       MOV            AX, %s
+                                       MOV            BX, %s
+                                       MUL            AX, BX
+                            """, left, right));
+                    }
+                    break;
+                }
+                return "AX";
+            }
+
+        }
     }
 }
